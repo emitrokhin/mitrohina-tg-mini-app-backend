53b0b76760ca577700a715c05cbb62056b03da53
Evgenii Mitrokhin <10193179+emitrokhin@users.noreply.github.com>
2024-04-30 19:44:58 +0300

    Feature 1 (#2)
    * Add spring boot starter files
    * Add vue.js starter files
    * Add git ignore rules for Java, Vue.js, IDEA

---
+++ src/main/java/ru/emitrohin/privateclubbackend/PrivateclubbackendApplication.java
@@ -0,0 +1,13 @@
+package ru.emitrohin.privateclubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateclubbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateclubbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/emitrohin/privateclubbackend/PrivateclubbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.emitrohin.privateclubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateclubbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
