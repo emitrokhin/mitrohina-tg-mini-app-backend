95006f26594eb518b607a09a1d249b7d3cded681
Evgenii Mitrokhin <emitrohin@icloud.com>
2024-05-18 15:53:03 +0300

    S3 and User utilities

---
+++ src/main/java/ru/emitrohin/privateclubbackend/util/S3Utils.java
@@ -0,0 +1,107 @@
+package ru.emitrohin.privateclubbackend.util;
+
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.Named;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class S3Utils {
+
+    private final S3Presigner s3Presigner;
+
+    private final S3Client s3Client;
+
+    private final String s3url;
+
+    private final String bucketName;
+
+    @Named("generatePresignedUrl")
+    public String generatePresignedUrl(String objectKey) {
+        URL url;
+        try (s3Presigner) {
+
+            var getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(objectKey)
+                    .build();
+
+            var getObjectPresignRequest = GetObjectPresignRequest.builder()
+                    .getObjectRequest(getObjectRequest)
+                    .signatureDuration(Duration.ofHours(1)) //TODO уменьшить срок
+                    .build();
+
+            url = s3Presigner.presignGetObject(getObjectPresignRequest).url();
+        }
+
+        return url.toString();
+    }
+
+    @Named("generatePermanentUrl")
+    public String generatePermanentUrl(String objectKey) {
+        return String.format("%s/%s", s3url, objectKey);
+    }
+
+    public String uploadPrivateFile(MultipartFile file) {
+        return uploadFileWithAcl(file, ObjectCannedACL.PRIVATE);
+    }
+
+    public String uploadPublicFile(MultipartFile file) {
+        return uploadFileWithAcl(file, ObjectCannedACL.PUBLIC_READ);
+    }
+
+    private String uploadFileWithAcl(MultipartFile file, ObjectCannedACL acl) {
+        String objectKey = generateObjectKey();
+        uploadFileToS3(objectKey, file);
+        setObjectRights(objectKey, acl);
+        return objectKey;
+    }
+
+    private String generateObjectKey() {
+        return UUID.randomUUID().toString();
+    }
+
+    private void uploadFileToS3(String objectKey, MultipartFile file) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .contentType(file.getContentType())
+                .build();
+        //TODO log successful operetaion
+        try {
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to upload file ", e);
+        }
+    }
+
+    private void setObjectRights(String objectKey, ObjectCannedACL right) {
+        PutObjectAclRequest request = PutObjectAclRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .acl(right)
+                .build();
+
+        s3Client.putObjectAcl(request);
+    }
+
+    public void deleteFile(String objectKey) {
+        var deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .build();
+
+        s3Client.deleteObject(deleteObjectRequest);
+    }
+}
---
+++ src/main/java/ru/emitrohin/privateclubbackend/util/UserUtils.java
@@ -0,0 +1,17 @@
+package ru.emitrohin.privateclubbackend.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class UserUtils {
+    public static UUID getCurrentUserId() {
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(Authentication::getPrincipal)
+                .map(Object::toString)
+                .map(UUID::fromString)
+                .orElseThrow(() -> new RuntimeException("User is not authenticated")); //TODO свой тип исключения?
+    }
+}
